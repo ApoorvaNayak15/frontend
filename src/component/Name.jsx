15798ef246f0fbb118e067b42e3b9af5844ff8bd
ApoorvaNayak15 <ApoorvaNayak2345@gmail.com>
2023-04-11 23:35:02 +0530

    components

---
+++ src/component/Name.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Name() {
+  return (
+    <div>
+        <h1>hello</h1>
+        <h2>world</h2>
+        <h3>morning</h3>
+        <h4>evening</h4>
+        <h4>hau</h4>
+        <h5>fan</h5>
+        <h6>sun</h6>
+        <p>children</p>
+        <span>+</span>
+        <b>bold</b>
+    </div>
+    
+  )
+}
+
+export default Name
